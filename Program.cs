cd9268f80e0e58842e81d45263e8a2eb7b7a4361
NotThanith <148561466+NotThanith@users.noreply.github.com>
2023-11-07 21:05:11 +0700

    I change the name of it

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace cafe_pos_system
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmlogin());
+            Application.Run(new frmLogin());
         }
     }
 }
